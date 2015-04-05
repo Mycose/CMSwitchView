8f61eb60e157fa87a3d7fd53a865f41ae54f519c
Mycose <morissard@gmail.com>
2015-04-05 17:16:32 +0200

    comments

--- CMSwitchViewExample/CMSwitchViewExample/CMSwitchView.h
+++ CMSwitchViewExample/CMSwitchViewExample/CMSwitchView.h
@@ -9,18 +9,30 @@
 #import <UIKit/UIKit.h>
 
 @protocol CMSwitchViewDelegate <NSObject>
+/// Called when the switch view is clicked or when you move the dot after the middle with the pan gesture
 - (void)switchValueChanged:(BOOL)value;
 @end
 
 @interface CMSwitchView : UIView<UIGestureRecognizerDelegate>
+/// If you want to have rounded dots and switch view or not, YES by default
 @property (nonatomic, assign) BOOL isRounded;
+/// Width of the border, 1 by default
 @property (nonatomic, assign) CGFloat borderWidth;
+/// color of the border, white by default
 @property (nonatomic, assign) UIColor* borderColor;
+/// color of the background of the switch view, clearColor by default
 @property (nonatomic, strong) UIColor* color;
+/// color of the background of the switch view when switched, clearColor by default
 @property (nonatomic, strong) UIColor* tintColor;
+/// width and height of the dot, frameHeight-2 by default
 @property (nonatomic, assign) CGFloat dotWeight;
+/// color of the dot, white by default
 @property (nonatomic, strong) UIColor* dotColor;
+/// duration of the animation, 0.6 by default
 @property (nonatomic, assign) CGFloat animDuration;
+/// delegate to be set
 @property (nonatomic, assign) id<CMSwitchViewDelegate> delegate;
+
+/// setup and build the views, set all the properties you want, then setup
 - (void)setup;
 @end
